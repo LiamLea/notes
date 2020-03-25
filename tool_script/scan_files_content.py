100e43098de640ca7c7b182e3bdf9c99e90b9cc0
Your Name <you@example.com>
2020-03-25 22:13:24 +0800

    3.25

--- tool_script/scan_files_content.py
+++ tool_script/scan_files_content.py
@@ -7,21 +7,24 @@ def scan_files_content(dir, search_pattern):
     ret = {}
     pattern = re.compile(search_pattern)
     for path, folders, files in os.walk(dir):
-        for file in files:
-            item=os.path.join(path,file)
-            with open(item) as fobj:
-                for line in fobj:
-                    match = pattern.search(line)
-                    if match :
-                        if ret.get(item):
-                            ret[item].append(line.strip())
-                        else:
-                            ret[item] = [line.strip()]
+        try:
+            for file in files:
+                item=os.path.join(path,file)
+                with open(item) as fobj:
+                    for line in fobj:
+                        match = pattern.search(line)
+                        if match :
+                            if ret.get(item):
+                                ret[item].append(line.strip())
+                            else:
+                                ret[item] = [line.strip()]
+        except UnicodeDecodeError:
+            pass
     return ret
 
 if __name__ == '__main__':
     dir = r"/tmp/test"
-    search_pattern = "ab"
+    search_pattern = r"ab"
     results = scan_files_content(dir, search_pattern)
     for file in results:
         print(file)
